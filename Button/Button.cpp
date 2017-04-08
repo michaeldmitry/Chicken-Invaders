c7fa4fed0a1d2bd27f6d06cf469a71f4d1e6f675
Mohamed Tawfik A.R <mo95scow@aucegypt.edu>
2017-04-08 17:11:38 +0200

    Check the new comments in both .hpp and .cpp files

--- Button/Button.cpp
+++ Button/Button.cpp
@@ -5,8 +5,8 @@
 
 Button::Button()
 {
-    key;
-    text;
+    key;    // This line does nothing. Remove it.
+    text;   // This line does nothing. Remove it.
 }
 
 Button::Button(const sf::Vector2f& s, const sf::Color& c, const sf::Vector2f& p, const sf::Font& f, int ch, const std::string& str, const sf::Color& col, const sf::Vector2f& pos)
@@ -14,7 +14,7 @@ Button::Button(const sf::Vector2f& s, const sf::Color& c, const sf::Vector2f& p,
     setButtonSize(s);
     setButtonColor(c);
     setButtonPosition(p);
-
+    
     setTextFont(f);
     setTextCharacterSize(ch);
     setTextString(str);
@@ -22,7 +22,7 @@ Button::Button(const sf::Vector2f& s, const sf::Color& c, const sf::Vector2f& p,
     setTextPosition(pos);
 }
 
-void Button::setButton(const sf::Vector2f& s, const sf::Color& c, const sf::Vector2f& p)
+void Button::setButton(const sf::Vector2f& s, const sf::Color& c, const sf::Vector2f& p) // This function sets multiple things at once - remove it.
 {
     setButtonSize(s);
     setButtonColor(c);
@@ -31,7 +31,7 @@ void Button::setButton(const sf::Vector2f& s, const sf::Color& c, const sf::Vect
 
 void Button::setButtonSize(const sf::Vector2f& s)
 {
-    key.setSize(sf::Vector2f(s));
+    key.setSize(sf::Vector2f(s));   // No need to write sf::Vector2f(s) .. just write s
 }
 void Button::setButtonColor(const sf::Color& c)
 {
@@ -43,7 +43,7 @@ void Button::setButtonPosition(const sf::Vector2f& p)
     key.setPosition(p);
 }
 
-void Button::setText(const sf::Font& f, int ch, const std::string& str, const sf::Color& col, const sf::Vector2f& pos)
+void Button::setText(const sf::Font& f, int ch, const std::string& str, const sf::Color& col, const sf::Vector2f& pos) // This function sets multiple things at once - remove it.
 {
     setTextFont(f);
     setTextCharacterSize(ch);
@@ -56,23 +56,23 @@ void Button::setTextFont(const sf::Font& f)
 {
     text.setFont(f);
 }
-    
+
 void Button::setTextCharacterSize(int ch)
 {
     text.setCharacterSize(ch);
 }
-    
+
 void Button::setTextString(const std::string& str)
 {
     text.setString(str);
 }
-    
+
 void Button::setTextColor(const sf::Color& col)
 {
     text.setFillColor(col);
 }
-    
-void Button::setTextPosition(const sf::Vector2f& pos)
+
+void Button::setTextPosition(const sf::Vector2f& pos)   // Does it make sense to put the text and key in different positions?
 {
     text.setPosition(pos);
 }
@@ -81,40 +81,40 @@ const sf::Vector2f& Button::getButtonSize() const
 {
     return key.getSize();
 }
-    
+
 const sf::Color& Button::getButtonFillColor() const
 {
     return key.getFillColor();
 }
-    
+
 const sf::Vector2f& Button::getButtonPosition() const
 {
     return key.getPosition();
 }
-    
+
 int Button::getTextCharacterSize() const
 {
     return text.getCharacterSize();
 }
-    
+
 const std::string& Button::getTextString() const
 {
     return text.getString();
 }
-    
+
 const sf::Color& Button::getTextColor() const
 {
     return text.getFillColor();
 }
-    
-const sf::Vector2f& Button::getTextPosition() const
+
+const sf::Vector2f& Button::getTextPosition() const // Redundant
 {
     return text.getPosition();
 }
-    
+
 Button::~Button()
 {
 }
 
 
-   
+
--- Button/Button.hpp
+++ Button/Button.hpp
@@ -5,17 +5,17 @@
 #include "ResourcePath.hpp"
 #include <stdio.h>
 
-// You shouldn't really give the user any hints of how the Button is implemented. The user shouldn't know about the RectangleShape and Text, only the Button as a whole. Check the comments marked (1)
+// Add setTexture and getTexture functions. Add drawButton function.
 
 class Button
 {
 private:
-<<<<<<< HEAD
     sf::RectangleShape key;
     sf::Text text;
 public:
     Button();
     Button(const sf::Vector2f& s, const sf::Color& c, const sf::Vector2f& p, const sf::Font& f, int ch, const std::string& str, const sf::Color& col, const sf::Vector2f& pos);
+    ~Button();
     
     void setButton(const sf::Vector2f& s, const sf::Color& c, const sf::Vector2f& p);
     void setButtonSize(const sf::Vector2f& s);
@@ -32,43 +32,11 @@ public:
     const sf::Vector2f& getButtonSize() const;
     const sf::Color& getButtonFillColor() const;
     const sf::Vector2f& getButtonPosition() const;
-
+    
     int getTextCharacterSize() const;
     const std::string& getTextString() const;
     const sf::Color& getTextColor() const;
     const sf::Vector2f& getTextPosition() const;
-
-=======
-    sf::RectangleShape button;  // Think of renaming it as to not get confused between Button and button
-    sf::Text text;
-public:
-    Button();
-    Button(sf::RectangleShape b, sf::Text t); // (1)
     
-    void setButton(sf::RectangleShape b); // (1)
-    void setButtonSize(sf::Vector2f& s); // const reference
-    void setButtonColor(sf::Color& c); // const reference
-    void setButtonPosition(int& p); // Should the parameter be an int?
-    
-    void setText(sf::Text t); // (1)
-    void setTextFont(sf::Font& f); // const reference
-    void setTextCharacterSize(int& ch); // no need for reference for simple types like int
-    void setTextString(std::string& str); // const reference
-    void setTextColor(sf::Color& col); // const reference
-    void setTextPosition(int& pos); // Should the parameter be an int?
-    
-    sf::Vector2f getButtonSize() const; // const reference return type
-    sf::Color getButtonColor() const; // Rename to getButtonFillColor - const reference return type
-    sf::Vector2f getButtonPosition() const; // const reference return type
-
-    int getTextCharacterSize() const;
-    std::string getTextString() const; // const reference return type
-    sf::Color getTextColor() const; // const reference return type
-    sf::Vector2f getTextPosition() const; // const reference return type
-    
-    sf::RectangleShape getButton() const; // (1)
-    sf::Text getText() const; // (1)
->>>>>>> aa304019cc3751c716ef9312dd7bdae2a7bfe3a1
-    ~Button();
 };
 #endif /* Button_hpp */
